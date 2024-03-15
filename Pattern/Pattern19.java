c3d241cd115824aefcbb02403fd0f32e5d988896
iamvishalaggarwal <vishalagarwal.872632@gmail.com>
2024-03-15 09:18:06 +0530

    Fix pattern 19

--- Pattern/Pattern19.java
+++ Pattern/Pattern19.java
@@ -8,31 +8,34 @@ public class Pattern19 {
         System.out.print("Enter any number = ");
         int num = sc.nextInt();
 
-        for (int i = 1; i < num; i++) {
-            for (int j = 1; j <= num - i; j++) {
+        for (int i = 0; i < num; i++) {
+            for (int j = 0; j < num - i; j++) {
                 System.out.print("*");
             }
-            for (int space = 1; space <= 2 * i - 2; space++) {
+            for (int space = 0; space < 2 * i; space++) {
                 System.out.print(" ");
             }
-            for (int j = 1; j <= num - i; j++) {
+            for (int j = 0; j < num - i; j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
 
-        // for (int i = 1; i < num; i++) {
-        // for (int j = 1; j <= i; j++) {
-        // System.out.print("*");
-        // }
-        // for (int space = 2 * (num - i + 1) - 2; space >= i; space--) {
-        // System.out.print(" ");
-        // }
-        // // for (int j = 1; j <= i; j++) {
-        // // System.out.print("*");
-        // // }
-        // System.out.println();
-        // }
+        for (int i = 0; i < num; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            // space
+            for (int space = num * 2 - 2; space > 2 * i; space--) {
+                System.out.print(" ");
+            }
+
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
         sc.close();
     }
 }
