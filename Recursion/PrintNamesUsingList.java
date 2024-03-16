7c18a512cdcc40927eb9bfabb324f775a5ce12ff
iamvishalaggarwal <vishalagarwal.872632@gmail.com>
2024-03-16 22:58:54 +0530

    step 1.5 in progress

---
+++ Recursion/PrintNamesUsingList.java
@@ -0,0 +1,32 @@
+package Recursion;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class PrintNamesUsingList {
+
+    public static List<String> printNtimes(int n) {
+        List<String> strList = new ArrayList<>();
+        printFunc(n, strList, "Coding Ninjas");
+        return strList;
+    }
+
+    public static void printFunc(int n, List<String> arr, String value) {
+        if (n == 0) {
+            return;
+        } else {
+            arr.add(value);
+            printFunc(n - 1, arr, value);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a numbers = ");
+        int num = sc.nextInt();
+        List<String> strList = printNtimes(num);
+        strList.forEach(val -> System.out.print(val + " "));
+        sc.close();
+    }
+}
---
+++ Recursion/PrintOneToNUsingArray.java
@@ -0,0 +1,32 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class PrintOneToNUsingArray {
+
+    public static int[] printNos(int x) {
+        int[] arr = new int[x];
+        num(x, arr);
+        return arr;
+    }
+
+    public static void num(int x, int[] arr) {
+        if (x == 0) {
+            return;
+        } else {
+            arr[x - 1] = x;
+            num(x - 1, arr);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a numbers = ");
+        int num = sc.nextInt();
+        int arr[] = printNos(num);
+        for (int val : arr) {
+            System.out.print(val + " ");
+        }
+        sc.close();
+    }
+}
