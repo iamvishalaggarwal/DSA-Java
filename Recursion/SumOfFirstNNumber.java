778f121c01c215e2263e1b1b2aaa33beb722a8c0
iamvishalaggarwal <vishalagarwal.872632@gmail.com>
2024-03-17 00:02:13 +0530

    step 1.5 in progress

---
+++ Recursion/SumOfFirstNNumber.java
@@ -0,0 +1,25 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class SumOfFirstNNumber {
+
+    // functional recursion
+    public static long getSumOfFirstNNumber(long num) {
+        if (num <= 0) {
+            return 0;
+        }
+        return num + getSumOfFirstNNumber(num - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a numbers = ");
+        long num = sc.nextLong();
+
+        long sum = getSumOfFirstNNumber(num);
+        System.out.println("Total = " + sum);
+        sc.close();
+    }
+
+}
