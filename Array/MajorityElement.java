4e6c32e7acd0a1931b32fecb12a13d3261f288a3
iamvishalaggarwal <vishalagarwal.872632@gmail.com>
2024-04-04 17:58:42 +0530

    Added array medium

--- Array/MajorityElement.java
+++ Array/MajorityElement.java
@@ -1,6 +1,6 @@
 package Array;
 
-import java.util.HashMap;
+// import java.util.HashMap;
 
 // Problem: The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
 // Input: nums = [2,2,1,1,1,2,2]
@@ -8,26 +8,53 @@ import java.util.HashMap;
 public class MajorityElement {
 
     /*
-     * Better approach: Using Hashing
-     * TC: O(n) + O(nlogn)
-     * SC: O(n)
+     ** Better approach: Using Hashing
+     ** TC: O(n) + O(nlogn)
+     ** SC: O(n)
+     * public static int majorityElement(int[] nums) {
+     * HashMap<Integer, Integer> map = new HashMap<>();
+     * for (int val : nums) {
+     * if (map.containsKey(val)) {
+     * map.put(val, map.get(val) + 1);
+     * } else {
+     * map.put(val, 1);
+     * }
+     * }
+     * int n = nums.length;
+     * for (int val : map.keySet()) {
+     * if (map.get(val) > n / 2) {
+     * return val;
+     * }
+     * }
+     * return 0; // never reach
+     * }
+     */
+
+    /*
+     * Optimal Approach
+     * Moore's Voting Algo
+     * TC: O(n) + O(n)
+     * SC: O(1)
      */
     public static int majorityElement(int[] nums) {
-        HashMap<Integer, Integer> map = new HashMap<>();
+        int count = 0;
+        int element = 0;
         for (int val : nums) {
-            if (map.containsKey(val)) {
-                map.put(val, map.get(val) + 1);
+            if (count == 0) { // when count becomes zero
+                element = val;
+                count = 1;
+            } else if (element == val) {
+                count++; // for same element
             } else {
-                map.put(val, 1);
+                count--; // for different element
             }
         }
-        int n = nums.length;
-        for (int val : map.keySet()) {
-            if (map.get(val) > n / 2) {
-                return val;
-            }
+        int count1 = 0;
+        for (int val : nums) {
+            if (element == val)
+                count1++;
         }
-        return 0; // never reach
+        return count1 > nums.length / 2 ? element : -1;
     }
 
     public static void main(String[] args) {
